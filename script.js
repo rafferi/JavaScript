0e81dceb4a64c2b08f514c0a021fc0a8c793a7db
rafferi <perepelkinvladislav74@gmail.com>
2024-09-27 09:41:36 +0500

    zad1-7

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-document.write('Мороз и солнце; день чудесный! <br>'); 
-document.write('Еще ты дремлешь, друг прелестный — <br>');
-document.write('Пора, красавица, проснись: <br>');
-document.write('Открой сомкнуты негой взоры <br>');
-document.write('Навстречу северной Авроры, <br>');
-document.write('Звездою севера явись! <br>');
+// document.write('Мороз и солнце; день чудесный! <br>'); 
+// document.write('Еще ты дремлешь, друг прелестный — <br>');
+// document.write('Пора, красавица, проснись: <br>');
+// document.write('Открой сомкнуты негой взоры <br>');
+// document.write('Навстречу северной Авроры, <br>');
+// document.write('Звездою севера явись! <br>');
+
+const name = 'Василий';
+const login = 'vasya123'; 
+const age = '18';
+const email = 'vasya123@mail.ru';
+
+
+
+    // выведем значения переменных
+    console.log("Исходные значения переменных");
+    console.log('Имя: ' + name);
+    console.log('Логин:' + login);
+    console.log('Возраст:' + age);
+    console.log('E-mail:' + email);
+    
+    // переопределим значения переменных
+    let newName = 'Анна';
+    let newLogin = 'anyta987';
+    let newAge = '17';
+    let newEmail = 'anyta987@mail.ru';
+    
+    // выведем новые значения переменных
+    console.log("Новые значения переменных");
+    console.log('Имя: ' + newName);
+    console.log('Логин:' + newLogin);
+    console.log('Возраст:' + newAge);
+    console.log('E-mail:' + newEmail);
